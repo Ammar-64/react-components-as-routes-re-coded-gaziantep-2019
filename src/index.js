10ffc2d398f18a7d9f9114b6e1df549f026a56fc
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-08-08 10:22:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -37,9 +37,9 @@ const Login = () => {
 
 ReactDOM.render((
   <Router>
-       <Route path="/" render={Home} />
-       <Route exact path="/about" render={About} />
-       <Route exact path="/login" render={Login} />
-   </Router>),
+      <Route path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+  </Router>),
   document.getElementById('root')
 );
