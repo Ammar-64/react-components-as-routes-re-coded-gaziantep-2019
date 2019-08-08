7cc0214a7e43b6680430866ec5d38f26000cd415
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-08-08 10:34:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Component/Home';
+import Home from './Components/Home';
 
 const About = () => {
   return (
